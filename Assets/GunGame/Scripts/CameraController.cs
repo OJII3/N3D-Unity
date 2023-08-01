2a8c3cf42d2f347dedd87891d8f2f3ca4d7251f8
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-08-01 15:30:16 +0900

    [add]: skateboard

--- Assets/GunGame/Scripts/CameraController.cs
+++ Assets/GunGame/Scripts/CameraController.cs
@@ -1,6 +1,9 @@
+using System;
 using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
 
 namespace GunGame
 {
@@ -14,14 +17,25 @@ namespace GunGame
         {
             playerInputActions = new PlayerInputActions();
             playerInputActions.Enable();
-            freeLookCamera.m_Lens.FieldOfView = initialFOV;
 
             // playerInputActions.Player.Zoom.performed += OnZoom;
         }
 
+        private void OnDestroy()
+        {
+            playerInputActions.Disable();
+            playerInputActions.Dispose();
+        }
+
         private void OnZoom(InputAction.CallbackContext ctx)
         {
-            freeLookCamera.m_Lens.FieldOfView += ctx.ReadValue<float>() * 0.1f;
+            freeLookCamera.m_Lens.FieldOfView += ctx.ReadValue<float>();
+        }
+
+        private void LateUpdate()
+        {
+            freeLookCamera.m_XAxis.Value += playerInputActions.Player.Look.ReadValue<Vector2>().x * 0.5f;
+            Cursor.lockState = CursorLockMode.Confined;
         }
     }
 }
